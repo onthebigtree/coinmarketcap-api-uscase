006d93b612d9532ef9367fb32de21c781401e392
Yewlne <60813639+onthebigtree@users.noreply.github.com>
2023-06-29 23:18:28 +0800

    更新注释，编写 README

---
+++ README.md
@@ -0,0 +1,21 @@
+# CoinMarketcap API Usage
+
+这个项目使用 Google Apps Script 和 CoinMarketCap 的 API，将特定加密货币（BTC 和 ETH）的价格信息写入 Google Sheets。
+
+## 如何使用
+
+### 1. 获取 CoinMarketCap API 密钥
+
+首先，你需要在 [CoinMarketCap](https://pro.coinmarketcap.com/) 上注册并获取 API 密钥。
+
+### 2. 打开 Google Sheets
+
+在你的 Google Drive 上创建一个新的 Google Sheets 文件。
+
+### 3. 打开 Google Apps Script 编辑器
+
+在 Google Sheets 中，点击 "扩展程序" -> "Apps Script" 打开 Google Apps Script 编辑器。
+
+### 4. 复制并粘贴脚本
+
+将 ``main.js`` 的代码复制并粘贴到 Google Apps Script 编辑器中：
--- main.js
+++ main.js
@@ -1,34 +1,41 @@
+// 该函数获取特定的加密货币数据，并将数据写入 Google Sheets
 function getSpecificCoinData() {
-  var coins = ['bitcoin', 'ethereum']; // 根据需要修改这个数组，添加你感兴趣的货币
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var coins = ['bitcoin', 'ethereum']; // 在这个数组中列出你感兴趣的货币
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet(); // 获取当前活动的工作表
 
+  // 遍历数组中的每一种货币
   for (var j = 0; j < coins.length; j++) {
+    // 为每种货币创建 URL
     var url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=' + coins[j];
     var headers = {
       'X-CMC_PRO_API_KEY': 'your-api-key', // 请替换成你的 CoinMarketCap API 密钥
       'Accept': 'application/json'
     };
 
+    // 设置请求选项
     var options = {
-      'method' : 'get',
-      'headers': headers,
-      'muteHttpExceptions': true
+      'method' : 'get', 
+      'headers': headers, 
+      'muteHttpExceptions': true 
     };
   
     var response = UrlFetchApp.fetch(url, options);
     var json = JSON.parse(response.getContentText());
-    var data = json.data;
+    var data = json.data; // 获取数据
 
+    // 遍历数据中的每一项
     var coinKeys = Object.keys(data);
     for(var i = 0; i < coinKeys.length; i++) {
+      // 将每一项的名字和美元价格添加到工作表的新一行
       sheet.appendRow([data[coinKeys[i]].name, data[coinKeys[i]].quote.USD.price]);
     }
   }
 }
 
+// 该函数创建一个触发器，每 20 分钟运行一次 getSpecificCoinData 函数
 function createTrigger() {
-  ScriptApp.newTrigger('getSpecificCoinData')
-    .timeBased()
-    .everyMinutes(20) // 设置为每20分钟运行一次
-    .create();
+  ScriptApp.newTrigger('getSpecificCoinData') // 创建一个新的触发器，触发函数是 getSpecificCoinData
+    .timeBased() // 触发器是基于时间的
+    .everyMinutes(20) // 设置为每 20 分钟运行一次
+    .create(); // 创建触发器
 }
